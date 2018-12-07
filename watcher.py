750f0db8c14f0b497e43a1f5bdfe5c0b9d82d763
phl4nk <phl4nk@gmail.com>
2018-12-07 20:33:03 +0000

    reworked a little, basic functionality complete

--- watcher.py
+++ watcher.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python
-# Manditory ASCII Logo
+#
+#  ReddiTWatch3r
+#      ___
+#    [|   |=|{)__
+#     |___| \/   )
+#      /|\      /|
+#     / | \    | \
 #
 # Author: phl4nk
 # Date: 07/12/2018
-# Version: 0.0.1
+# Version: 0.0.5
 
-import urllib2,re,ast
+import urllib2,re,datetime
 
-online_regex = 's1wbv0ui-12 ffQWxW">([^<]*)<\/p>'
+class_regex = 's1wbv0ui-12 ffQWxW">([^<]*)<\/p>'
+request_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"}
 
+# pull HTML from subreddit
 def grab_subreddit(subreddit):
     try:
-        request_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"}
         request = urllib2.Request("https://www.reddit.com/r/"+subreddit, headers=request_headers)
         contents = urllib2.urlopen(request).read()
         return contents
@@ -19,12 +26,17 @@ def grab_subreddit(subreddit):
         print "[!] Crawling failed:", e
         return None
 
-def get_online(html_data):
+# parse total amount of subscribers, and currently online
+# should return a list of subs,curr_online
+def get_stats(html_data):
     try:
-        regex=re.compile(online_regex)
-        return value_to_int(regex.findall(html_data)[0]);
+        regex=re.compile(class_regex)
+        counts = regex.findall(html_data)
+        total_subs = str(value_to_int(counts[0]))
+        curr_online = str(value_to_int(counts[1]))
+        return [total_subs,curr_online]
     except Exception, e:
-        print 'Cannot find view count from response'
+        print '[!] Something went wrong',e
 
 # Custom function to turn strings into ints
 # https://stackoverflow.com/questions/39684548/convert-the-string-2-90k-to-2900-or-5-2m-to-5200000-in-pandas-dataframe
@@ -41,8 +53,17 @@ def value_to_int(x):
                 return int(float(x.replace('m', '')) * 1000000)
         return 0
 
-#print value_to_int('1.2k')
-#print get_online(grab_subreddit("netsec"))
-#print get_online(grab_subreddit("conspiracy"))
-#print get_online(grab_subreddit("gif"))
-#print get_online(grab_subreddit("redteam"))
+# save to CSV format for ease of ingest/graphing later
+# data should be a list with 2 items
+def save_data(data,filename):
+    t = datetime.datetime.now().strftime('%Y-%m-%d,%H:%M:%S')
+    f = open(filename+'.txt','a')
+    f.write(t+','+data[0]+','+data[1]+'\n')
+    f.close()
+
+def watch(subreddit):
+    data = grab_subreddit(subreddit)
+    output = get_stats(data)
+    save_data(output,subreddit)
+
+watch('netsec')
