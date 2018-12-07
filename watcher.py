c8c059290d7cb8ba876251452f36b3c4d6cf6511
phl4nk <phl4nk@gmail.com>
2018-12-07 21:33:52 +0000

    doh...more betterer now

--- watcher.py
+++ watcher.py
@@ -9,17 +9,17 @@
 #
 # Author: phl4nk
 # Date: 07/12/2018
-# Version: 0.0.7
+# Version: 0.1.0
 
 import urllib2,re,datetime,time
 
-class_regex = 's1wbv0ui-12 ffQWxW">([^<]*)<\/p>'
+viewing_regex = 'currentlyViewingCount":(\d+),'
+subs_regex = 'subscribersCount":(\d+),'
 request_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"}
 
 # pull HTML from subreddit
 def grab_subreddit(subreddit):
     try:
-        print '[+] Crawling subreddit'
         request = urllib2.Request("https://www.reddit.com/r/"+subreddit, headers=request_headers)
         contents = urllib2.urlopen(request).read()
         return contents
@@ -28,38 +28,23 @@ def grab_subreddit(subreddit):
         return None
 
 # parse total amount of subscribers, and currently online
-# should return a list of subs,curr_online
+# should return a list of total_subs,curr_viewing
 def get_stats(html_data):
     try:
-        regex=re.compile(class_regex)
-        counts = regex.findall(html_data)
-        total_subs = str(value_to_int(counts[0]))
-        curr_online = str(value_to_int(counts[1]))
-        return [total_subs,curr_online]
+        Vregex = re.compile(viewing_regex)
+        Sregex = re.compile(subs_regex)
+        total_subs = str(Sregex.findall(html_data)[0])
+        curr_viewing = str(Vregex.findall(html_data)[0])
+        return [total_subs,curr_viewing]
     except Exception, e:
         print '[!] Something went wrong',e
-
-# Custom function to turn strings into ints
-# https://stackoverflow.com/questions/39684548/convert-the-string-2-90k-to-2900-or-5-2m-to-5200000-in-pandas-dataframe
-# There is a legit reason to do int(float()) - edge case: '1.2k'
-def value_to_int(x):
-    try:
-        return int(float(x))
-    except ValueError:
-        if 'k' in x:
-            if len(x) > 1:
-                return int(float(x.replace('k', '')) * 1000)
-        if 'm' in x:
-            if len(x) > 1:
-                return int(float(x.replace('m', '')) * 1000000)
-        return 0
 
 # save to CSV format for ease of ingest/graphing later
 # data should be a list with 2 items
 def save_data(data,filename):
     try:
         t = datetime.datetime.now().strftime('%Y-%m-%d,%H:%M:%S')
-        f = open(filename+'.txt','a')
+        f = open(filename+'.csv','a')
         f.write(t+','+data[0]+','+data[1]+'\n')
         print '[+] Written data to file:', data
         f.close()
@@ -73,5 +58,5 @@ def watch(subreddit):
     save_data(output,subreddit)
 
 while True:
-    watch('netsec')
-    time.sleep(5)
+    watch('news')
+    time.sleep(30)
